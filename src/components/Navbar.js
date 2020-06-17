ee78ef501b820a4f1d5a99ba60882a28fc0c71f5
Andrew A. Stenipanin <and5251@yandex.ru>
2020-06-18 01:30:55 +0300

    styled components, Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import { ButtonContainer } from './Button'
+import styled from "styled-components";
+
 
 const Navbar = (props) => {
   return (
-    <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+    <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
       {/* https://www.iconfinder.com/icons/1243689/call_phone_icon Creative
       Commons (Attribution 3.0 Unported); https://www.iconfinder.com/Makoto_msk
        */}
@@ -19,14 +22,25 @@ const Navbar = (props) => {
           </Link>
         </li>
       </ul>
-      <Link to='/cart' className="ml-auto">
-        <button>
-          <i className="fas fa-cart-plus" />
-          my cart
-        </button>
+      <Link to="/cart" className="ml-auto">
+        <ButtonContainer>
+          <span className="mr-2">
+            <i className="fas fa-cart-plus" />
+            my cart
+          </span>
+        </ButtonContainer>
       </Link>
-    </nav>
+    </NavWrapper>
   );
 };
 
 export default Navbar;
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite)!important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`
