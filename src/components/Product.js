9b71e12623136e1b71462c499efeaee95736c6e9
Andrew A. Stenipanin <and5251@yandex.ru>
2020-06-17 03:27:15 +0300

    first commit

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = props => {
+  return ( 
+    <div>
+      <h3>hello from single product</h3>
+    </div>
+   );
+}
+ 
+export default Product;
---
+++ src/components/ProductList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductList = props => {
+  return ( 
+    <div>
+      <h3>Hello from product list</h3>
+    </div>
+   );
+}
+ 
+export default ProductList;
